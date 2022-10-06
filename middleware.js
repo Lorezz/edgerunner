f89ccb4fb8005eb50f8c4d21085834dcc48abdde
Lorezz <point.point@gmail.com>
2022-10-06 12:42:51 +0200

    middleware basic

--- middleware.js
+++ middleware.js
@@ -3,10 +3,6 @@ import { NextResponse } from 'next/server';
 export function middleware(request) {
   const { geo, url, nextUrl } = request;
   const country = geo.country || 'US';
-  // console.log('middleware country ', country);
-  // request.nextUrl.searchParams.set('country', country);
-
-  // console.log(url, nextUrl);
 
   if (nextUrl.pathname.startsWith('/chi-siamo') && country === 'IT') {
     return NextResponse.rewrite(new URL('/about?locale=it', url));
